9eb96af5655cc1ee7189ab26e8caf6e0d5e7af86
Joe <zielinski@fastmail.com>
2021-05-20 14:31:53 +0200

    dun choke

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,10 +10,16 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
+}
+
+impl AppendBar for Vec<String> { 
+    fn append_bar(self) -> Self {
+        let mut r: Vec<String> = self.clone();
+        r.push(String::from("Bar"));
+        r 
+    }
 }
 
 //TODO: Add your code here
